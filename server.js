cb3a686ccdacdd87857f9b0f12149fd3785a960d
arpn-Goyal <arpangoyal95@gmail.com>
2025-03-23 15:52:07 +0530

    created writeBlog, updated server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import e from "express";
 import useSignupRouter from "./routes/signUp.js";
 import useLoginRouter from './routes/login.js';
 import useUserRouter from './routes/user.js';
+import bodyParser from "body-parser";
 
 const port = 5000;
 const app = e();
@@ -12,6 +13,10 @@ app.set('view engine', 'ejs');
 // To Serve Static Files
 // It allows your server to directly serve frontend assets like HTML, CSS, JavaScript, images, and fonts without defining separate routes.
 app.use(e.static('public'));
+// Role of body-parser in Express
+// body-parser is a middleware in Express.js that extracts the data sent in HTTP requests (like form submissions) and makes it available in req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // Routers
 app.use('/signup', useSignupRouter);
@@ -20,7 +25,7 @@ app.use('/user', useUserRouter);
 
 // ---------------------------------
 app.get('/', (req, res)=>{
-    res.render('blogs');
+    res.render('writeBlog');
 })
 
 app.listen(port, ()=>{
